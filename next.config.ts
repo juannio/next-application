18142f9c8489acb3a74a5bae0d2183a4d4d3eb00
juannio <juaniorosas.99@gmail.com>
2025-02-24 02:22:44 -0600

    Added docker file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   webpack: ( config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     return config
-  }
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
